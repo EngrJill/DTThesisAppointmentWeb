ce5e181f6b0fe06053caea2de0e586986696320f
Daniel A. Soriano II <72549944+EngrJill@users.noreply.github.com>
2022-02-18 13:30:19 +0800

    Working Backend and some adjustments

--- pages/appointment.vue
+++ pages/appointment.vue
@@ -4,11 +4,13 @@
             <div class="form-container">
             <h3>Hello, {{this.$route.params.nname}}. Kindly fill-out this form</h3>
             <p :style="{display: requiredTrigger}">All fields are required</p>
-                <form action="" class="form">
+                <form @submit.prevent="onCreatePost" class="form">
                     <label for="first_name">First Name</label><br>
                     <input type="text" placeholder="Input First Name" v-model="first_name"><br>
                     <label for="">Last Name</label><br>
                     <input type="text" placeholder="Input Last Name" v-model="last_name"><br>
+                    <label for="address">Address</label><br>
+                    <input type="text" placeholder="Input Address" v-model="address"><br>
                     <label for="">Contact Number</label><br>
                     <input type="number" placeholder="Input Contact Number" v-model="contact_number"><br>
                     <label for="">Email</label><br>
@@ -20,9 +22,12 @@
                     <label for="">Appointment date</label><br>
                     <date-picker style="height: 46px" v-model="time1" valueType="format" placeholder="Please pick a date"></date-picker><br>
                     <p :style="{display:dateChecker}">The Date must be a Future Date</p>
+                    <h1 v-if="isSuccess">Congratulations, Appointment Approved</h1>
+                    <p v-if="isSuccess">Please click the <strong>View QR</strong> to View your QR Code</p>
+                    <button :disabled="btnDisabled2" :style="{backgroundColor: btnColorDisabled2}" type="submit">Verify Appointment</button>
                     <nuxt-link :to="{ name: 'final', params: { pangalan: this.first_name, code: hashFunction(), time: convertToReadableDate(), date: this.time1  } }">
                         <button :disabled="btnDisabled" :style="{backgroundColor: btnColorDisabled}">
-                        PROCEED 
+                        View QR
                         </button>
                     </nuxt-link>
                 </form>
@@ -35,18 +40,21 @@
    import DatePicker from 'vue2-datepicker';
    import 'vue2-datepicker/index.css';
    import VueQr from 'vue-qr';
+   import axios from 'axios'
 
   export default {
     components: { DatePicker, VueQr },
     data() {
       return {
         time1: '',
+        address: '',
         first_name: '',
         last_name: '',
         contact_number: '',
         email: '',
         appointment_location: '',
         appointment_purpose: '',
+        isSuccess: false,
       };
     },
     computed: {
@@ -54,10 +62,12 @@
             if (this.time1.length != 0 && 
                 this.first_name.length != 0 &&
                 this.last_name.length != 0 &&
+                this.address.length != 0 &&
                 this.contact_number.length != 0 &&
                 this.email.length != 0 &&
                 this.appointment_location.length != 0 &&
                 this.appointment_purpose.length != 0 &&
+                this.isSuccess === true &&
                 (+new Date() < +new Date(this.time1))
             ) {
                 return false
@@ -70,10 +80,12 @@
             if (this.time1.length != 0 && 
                 this.first_name.length != 0 &&
                 this.last_name.length != 0 &&
+                this.address.length != 0 &&
                 this.contact_number.length != 0 &&
                 this.email.length != 0 &&
                 this.appointment_location.length != 0 &&
                 this.appointment_purpose.length != 0 &&
+                this.isSuccess === true &&
                 (+new Date() < +new Date(this.time1))
 
             ) {
@@ -87,15 +99,54 @@
             if (this.time1.length != 0 && 
                 this.first_name.length != 0 &&
                 this.last_name.length != 0 &&
+                this.address.length != 0 &&
                 this.contact_number.length != 0 &&
                 this.email.length != 0 &&
                 this.appointment_location.length != 0 &&
+                this.isSuccess === true &&
                 this.appointment_purpose.length != 0
             ) {
                 return 'none'
             }
             else {
                 return 'block'
+            }
+        },
+        btnDisabled2: function() {
+            if (this.time1.length != 0 && 
+                this.first_name.length != 0 &&
+                this.last_name.length != 0 &&
+                this.address.length != 0 &&
+                this.contact_number.length != 0 &&
+                this.email.length != 0 &&
+                this.appointment_location.length != 0 &&
+                this.appointment_purpose.length != 0 &&
+                this.isSuccess === false &&
+                (+new Date() < +new Date(this.time1))
+            ) {
+                return false
+            }
+            else {
+                return true
+            }
+        },
+        btnColorDisabled2: function() {
+            if (this.time1.length != 0 && 
+                this.first_name.length != 0 &&
+                this.last_name.length != 0 &&
+                this.address.length != 0 &&
+                this.contact_number.length != 0 &&
+                this.email.length != 0 &&
+                this.appointment_location.length != 0 &&
+                this.appointment_purpose.length != 0 &&
+                this.isSuccess === false &&
+                (+new Date() < +new Date(this.time1))
+
+            ) {
+                return '#3598DC'
+            }
+            else {
+                return 'gray'
             }
         },
         dateChecker: function() {
@@ -157,7 +208,26 @@
                 let readableDate = month[readableMonth] + " " + timeVar.slice(8,10) + ", " + timeVar.slice(0,4)
 
                 return readableDate
-            }
+            },
+
+        onCreatePost() {
+            axios
+                .post('http://127.0.0.1:8000/api/user_details',{
+                              "name": this.first_name + " " + this.last_name,
+                              "address": this.address,
+                              "phoneNumber": this.contact_number,
+                              "email": this.email,
+                              "appointmentStart": this.time1 + " 00:00:00",
+                              "appointmentEnd": this.time1 + " 23:59:59",
+                              "placeAppointment": this.appointment_location,
+                              "purposeAppointment": this.appointment_purpose
+                              })
+                .then((response) => {
+                    this.isSuccess = true;
+                    console.log(response);
+                    this.$emit('postcreated');
+                });
+        },
     }
   };     
 </script>
